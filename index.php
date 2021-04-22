438810ceac3f59f54c3c1a62cf79d73839e43869
safwan39 <53286721+safwan39@users.noreply.github.com>
2021-04-22 10:55:10 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,12 @@
+<?php
+
+use RegexRouter;
+
+include 'Route.php';
+
+$route = new Route('/test');
+$route->get('/(\d+)', function($a, $b){ 
+    $a->get('/edit', function($a, $b){ return 'edit '.$b; });
+    return 'get '.$b; 
+})->post('/(\d+)$', function($a,$b){ return 'post '.$b; })
+->delete('/(\d+)$', function($a,$b){ return 'delete '.$b; });
